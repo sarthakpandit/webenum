cf3f4d6a7fd52fcba423df5a09aae646152c98f0
emilio.pinn <emilio.pinn@d5689014-ab77-0b10-0834-67ece9f60340>
2010-07-19 21:17:43 +0000

    Usage & Fixes

--- webenum.py
+++ webenum.py
@@ -12,6 +12,35 @@ import getopt, sys, urllib, urllib2, random, re, itertools, cookielib, urlparse,
 
 w_default = ["about","access","accesso", "accessi", "accounting", "account","accounts","accnt","adkit","admin", "adminlogin", "administracion","administrador","administrator","administrators","admins", "amministratore", "amministratori", "ads","affiliate","affiliates","afiliados", "affiliati", "agenda","agent","ajax","akamai","alerts","alpha","analyzer","announce","announcements","antivirus","apache","app","application","applications", "applicazioni", "apps","articolo", "articoli", "articles", "article", "auction","auth", "authenticate", "authentication", "autenticazione", "auto","av","b","back","backend","backup","banking","beta","billing","biz","blog","blogs","broadcast","bug","bugs","bugzilla","build","bulletins","buy","cache","calendar","careers","carrello", "carrelli","catalog","catalogo","cc", "carta", "carte", "cert","certificates", "certificato", "certificati","certify","certserv","certsrv","cgi","channel","channels","chat","chats","check","checkpoint","cisco","class","classes","classifieds","classroom","client", "cliente", "clienti", "clientes","clients","club","clubs","cluster","clusters","cmail","cms", "cms_users", "cms_passwords", "cms_accounts", "cms_admins", "cms_user", "cms_password", "cms_account", "cms_admin", "code","coldfusion","commerce","commerceserver","community","compras","concentrator","conference","conferencing","confidential","configuration", "configurazione", "config", "conf", "cfg", "connect","console","consult","consultant","consultants","consulting","consumer","contact", "contacts", "contatti", "content","contracts","core","corp","corpmail","corporate","correo","correoweb","courses","crm","css","customer","customers","cv","cvs","d","data", "dates","database01","database02","database1","database2","databases","datastore","datos","dati", "db","db0","db01","db02","db1","db2","dealers","def","default","delta","demo","demonstration","demos", "dimostrazione", "depot","design","designer","dev","devel","develop","developer","developers","development","device","devserver","devsql","dhcp","dial","dialup","digital","dir","direct","directory", "directories", "disc","discovery","discuss","discussion","discussions","disk", "disks", "distributer","distributers","dmail", "dnews","dns","do","docs","documentos","documents", "documentation", "documenti", "documento", "domain","domains","dominio","download","downloads","drupal","dyn","dynamic","e","e-com","e-commerce","echo","ecom","ecommerce","edu","ejemplo","esempio","email", "emails", "e-mail", "e-mails","employees","empresa","empresas","enable","eng","engine","engineer","engineering","enterprise","epsilon","estadisticas","esx","et","eta","europe","events","domain","exchange","extern","external","extranet","f","fax","feedback","feeds","field","file","files","fileserv","fileserver","filter","find","fix","fixes","flash","foobar","forum","forums","foto","fotos","foundry","freeware", "front","ftp","fw","galleria","galeria","galleries","gallery","games","gamma","gateway","gm","gmail","groups","guest","gw", "hash",  "hashes", "hello","help","helpdesk","helponline","hi","hidden","home","homes","host","hosts","hotel","howto","http","https","hub","humanresources","i","ids","iis","images","imail","img", "imgs","immagini", "image", "inc","include","incoming", "incomings", "info","inside","install","intern", "interno", "internal","international","internet","intl","intranet","invalid","investor","investors","invia","invio", "inviato", "inviati","io", "iscritto", "iscritti","jobs","job","kerberos","keynote","l","lab","laboratory","labs","lambda","lan","laptop","launch","ldap","legal","li","lib","library","link","lista", "liste", "lists", "list","live","load","local","localhost","log","log0","log1","log2","logfile","logfiles","logger","logging","loghost","login", "logins", "log-in", "logs", "logout", "loginout", "lotus","mac","master", "masters","mail","mailer","mailing","maillist","maillists","mailroom","mailserv","mailsite","mails","main","maint","mall","manage","management","manager","manufacturing","map","maps","marketing","marketplace","media", "medias", "member","members", "memberid", "members_id", "membri", "membro", "messages", "messaggi", "messaggio", "messenger","mirror","monitor","movies","mp3","mpeg","mpg","ms","msg", "msgs", "ms-exchange","ms-sql","msexchange","mssql","mssql0","mssql1","msysobject","multimedia","music","my","mysql","mysql0","mysql1","name","names", "nomi", "nome", "nat","net","netapp","netdata","netstats","network","new","news","novita", "notizia", "notizie", "newsfeed","newsfeeds","newsgroups","no","node","nomi", "nome", "noticias","null","office","offices","ok","old","online","open","operations","oracle","orders","out","outbound","outgoing","outlook","outside","p","page","pager","pages","pagine","pagina", "password", "pass", "pass_hash", "passw", "pword", "pwrd", "pwd", "pwds", "pw", "passes", "passwords", "partner","partners","patch","patches","pbx","pcmail", "personal", "personale","pgp","phi","phone","phones", "photos","pics","pictures", "pix","policy", "policies", "polls","pop","portal","portals","portfolio","post", "posts","posta","posta1","posta2","postoffice","press","printer","priv","privacy","private","privato","problems", "products", "prodotto", "prodotti","profiles", "profili", "profilo", "project","projects", "progetti", "progetto", "promo","proxy","prueba","prova","prove", "prova1", "prova2","pub","public","pubs","pubblico", "pubblici","pw","py","q","qmail","r","radius","read", "ricevuto", "ricevuti","ref","reference","reg","register","registro","registri","registry","regs","relay","rem","remote","reports","research","ricerca", "ricerche","reseller","reserved","resumenes","root","route","rs","rss","rw","s","s1","sadmin","safe","sales","scanner","schedules","sd","se","search", "searches", "sec","secret","secure","secured", "securid","security","sendmail","serv", "server","server1","servers","service","services", "servicio","servidor","setup", "settings", "setting", "shared","sharepoint","shareware","shipping","shop","shops","shoppers","shopping","sigma","sign","signin","signup","site","sms","smtp","snort","socal","soci", "socio","software","solutions","source","sourcecode","sources", "spam","sql","sqlserver","squid","ss","ssh","ssl","staff","stage","staging","start","stat","static","statistics","stats","stock","storage","store", "store1", "store2", "streaming","studio","submit","submission", "submissions", "subversion","sun","supplier","suppliers","support","sw","sysadmin", "sysadmins", "sysadm", "sysback","syslog","syslogs", "sysobjects", "system", "systems", "teams", "team","tech","techsupport","telephone","telephones","telephony","temp","temp1", "temp2", "terminal","testbed","testing","test","tests","testo","testserver","testsite","testsql","times","to", "todo","tool","tools","tracker","training","transfers", "trasferimenti","tumb","thumbnails", "thumbnail","tunnel","tv","updates","upload","uploads", "usr", "usrs", "usr1", "usr2", "user", "user1", "user2", "users","usuarios", "usuario", "utente", "utenti", "username", "user_name", "user_username", "uname", "usern", "user_password", "user_pass", "user_passw", "user_pwrd", "user_pwd", "user_id", "user_ids", "user_user", "user_users", "user_log","user_logs", "utilities","vend","vendors","venditori","video","videos","vm","vnc","voice","voicemail","voip","vpn","w", "wais","wallet","wap","w3c","web","webaccess","webadmin","webalizer","webboard","webcache","webcam","webcast","webdev","webdocs","webfarm","webhelp","weblib","weblogic","webmail","webmaster","webproxy","webs","webserv","webserver","webservices","website","websites","websphere","websrv","webstats","webstore","websvr","webtrends","welcome","whois","wiki","win","wlan","wordpress","work", "works","world","write","webserver", "ws","wusage","wv","ww","www","www-1", "www01", "www1","www2","www3","wwwdev","wwwmail","xmail","xml", "zone", "zones", "articoli"]
 
+usage = """
+WebEnum 0.1
+
+Usage:
+
+./webenum [-w wordlist] [-h header] [-d postdata] [-m match] url
+
+URL, headers and POST data can contain:
+
+  %%WORD%%, get strings from internal wordlist of ~900 common words.
+  %%WORD[0-9]%% get strings from wordlist file paths specified in -w option.
+  %%INT%%, generate integer ranges. Default: from 0 to 50.
+  %%CHAR%%, generate character and string ranges. Default: from 'a' to 'z'.
+  %%TABLE%%, generate 1,1,..,1 string, useful for SQL injection. Default: from 0 to 50.
+
+  INT, CHAR and TABLE can be customized using [end] or [start]:[end], like %%INT4:8%%.
+
+Match (-m)
+
+  Python statement to match correct responses. Variables are response and status_code. Examples:
+  "'Logged' in response and status_code == 200"
+  "'Wrong password' in response"
+
+POST datas (-d), headers (-h) and wordlists (-w):
+
+  -d "param1=value&param2=value&param3=%%WORD%%"
+  -h "User-Agent:Mozilla Firefox %%INT4:12%%.0" -h "Referer:%%WORD1%%"
+"""
+
 
 class struct:
   def __init__(self, **kwds):
@@ -169,10 +198,19 @@ class sqlenum:
 	opts, args = getopt.getopt(sys.argv[1:-1], 'w:m:v:d:h:c:', ['wordlist', 'match', 'verbose', 'data', 'headers', 'cookie'])
     except getopt.error, msg:
 	print "Error:", msg
+	print usage
 	exit(2)
+  
     
-    url=sys.argv[-1]
-   
+    
+    if sys.argv[-1][:4] == 'http':
+      url=sys.argv[-1]
+    else:
+      print '! Error, -u url required'
+      print usage
+      return -1      
+  
+  
     for o, a in opts:
       
 	if o in ("-m", "-match"):
@@ -188,10 +226,8 @@ class sqlenum:
 	  headers[h[0]]=h[1]
 	if o in ("-c", "-cookie"):
 	  cookiepath = a
-      
-    if not url:
-      print '! Error, -u url required'
-      return -1      
+    
+  
   
     if urllib2.getproxies():
       print '+ Using HTTP proxy ' + urllib2.getproxies()['http']
@@ -201,7 +237,7 @@ class sqlenum:
     if self.req.fword:
 
       if self.req.fword-self.req.def_wordlist != len(split_wlists):
-	print '! Error having ' + str(len(split_wlists)) + ' wordfile but not %%WORD' + str(len(split_wlists)) + '%% in parameters.' 
+	print '! Error having ' + str(len(split_wlists)) + ' wordfile and ' + str(self.req.fword-self.req.def_wordlist) + ' %%WORD%%s parameters.' 
 	return -1
         
       if self.req.def_wordlist==1 :
